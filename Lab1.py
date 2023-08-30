6f5b2b858cd748294326f5ccb1510e945d8e792e
Jake Cohen <46077226+JakeCohen0427@users.noreply.github.com>
2023-08-30 14:20:21 -0400

    Create Lab1.py

---
+++ Lab1.py
@@ -0,0 +1,14 @@
+#############################################################
+#
+#File name: Lab1.py
+#Programmer: Jake Cohen
+#Course: CSC1710
+#Date Submitted: August 25, 2023 
+#Location: /home/students/jcohen/csc1710/lab1/lab1.py
+#Description: Prints hello world!
+#Asisstance: None
+#Compile and Execute: Python3 lab1.py
+#Additional Files: None
+#
+#############################################################
+print("hello world!")
