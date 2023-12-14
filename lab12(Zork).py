2677af25d28126dd19a7b2e2571d4cd6870e821f
Jake Cohen <46077226+JakeCohen0427@users.noreply.github.com>
2023-12-14 17:26:49 -0500

    Add files via upload

---
+++ lab12(Zork).py
@@ -0,0 +1,21 @@
+print("d")
+
+#def getWeapon(weapon):
+ #  return "Sword"
+
+#def getGold():
+ #  return gold
+
+#def addGold(amtAdded):
+ #   gold = gold + amtAdded
+
+    
+#def viewPlayerStats():
+ #   weapon = ""
+  #  print("********************************")
+   # print("**     Hitpoints: " + playerHealth)
+    #print("**     Weapon: " + "getWeapon(weapon)")
+    #print("**     Gold: " + "getGold()")
+    #print("********************************")
+    
+#viewPlayerStats()
